2db2b4ac8a7dfdef98482fe4d19f0019268cf4d9
catherine <h1codingqueenh1@gmail.com>
2022-08-19 21:51:54 -0400

    Added XCode Project

---
+++ yu-shop-catherine/yu-shop-catherine/yu_shop_catherineApp.swift
@@ -0,0 +1,17 @@
+//
+//  yu_shop_catherineApp.swift
+//  yu-shop-catherine
+//
+//  Created by adobada on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct yu_shop_catherineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
