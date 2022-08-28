90a4af1a656dbb492b72422466dff8769807d24c
catherine <h1codingqueenh1@gmail.com>
2022-08-28 14:17:36 -0400

    Completed assignments 1-6 in playground

--- yu-shop-catherine/yu-shop-catherine/yu-shop-playground.playground/Contents.swift
+++ yu-shop-catherine/yu-shop-catherine/yu-shop-playground.playground/Contents.swift
@@ -1,11 +1,45 @@
-struct ProductsForSale {
-  
-  let catagory: String
-  var productName: String
-  var productSize: String
-  var color: String
-  var productQuantity: Int
-  var description: String
+// Assignment 1
+
+struct Product {
+  // Properties
+  let name: String
+  let size: Int
+  let color: String
+  // Assignment 3
+  let description: String?
 }
 
+// Assignment 2: Collection
+var myProductList = [Product]()
 
+// Assignment 4
+
+let product1 = Product(name: "Shorts", size: 1, color: "Indigo", description: "Denim. Made in Japan")
+let product2 = Product(name: "Pants", size: 3, color: "Black", description: "100% Cotton")
+let product3 = Product(name: "T-Shirt", size: 4, color: "White", description: "100% Jersey Cotton")
+let product4 = Product(name: "Mules", size: 38, color: "Walnut", description: nil)
+let product5 = Product(name: "Hat", size: 2, color: "Forest Green", description: nil)
+let product6 = Product(name: "Hoodie", size: 4, color: "Black", description: "100% French Terry Cotton")
+
+// Assignment 5
+func addProduct(product: Product) {
+  myProductList.append(product)
+}
+
+addProduct(product: product1)
+addProduct(product: product2)
+addProduct(product: product3)
+addProduct(product: product4)
+addProduct(product: product5)
+addProduct(product: product6)
+
+// Assignment 6
+func printElements(products: [Product]) {
+  for product in products {
+    if product.description != nil {
+      print(product)
+    }
+  }
+}
+
+printElements(products: myProductList)
