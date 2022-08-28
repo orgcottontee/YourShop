9d9cc06c531a7c4fdf96ae7e12023b122d90795c
catherine <h1codingqueenh1@gmail.com>
2022-08-28 09:22:59 -0400

    Added yu-shop-playground

---
+++ yu-shop-catherine/yu-shop-catherine/yu-shop-playground.playground/Contents.swift
@@ -0,0 +1,11 @@
+struct ProductsForSale {
+  
+  let catagory: String
+  var productName: String
+  var productSize: String
+  var color: String
+  var productQuantity: Int
+  var description: String
+}
+
+
