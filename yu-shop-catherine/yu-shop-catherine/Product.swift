014c4aff0c968e213ebde2b28c35156e8fca5e39
catherine <h1codingqueenh1@gmail.com>
2022-08-28 15:09:09 -0400

    Added Product file and displayed Product data on ContentView

--- yu-shop-catherine/yu-shop-catherine/Product.swift
+++ yu-shop-catherine/yu-shop-catherine/Product.swift
@@ -1,19 +1,32 @@
+//
+//  Product.swift
+//  yu-shop-catherine
+//
+//  Created by adobada on 8/28/22.
+//
+
+import Foundation
+
 // Assignment 1
+
 struct Product {
+  // Properties
   let name: String
   let size: Int
   let color: String
-  let description: String? // Assignment 3
+  // Assignment 3
+  let description: String?
 }
 
 // Assignment 2: Collection
 var myProductList = [Product]()
 
 // Assignment 4
+
 let product1 = Product(name: "Shorts", size: 1, color: "Indigo", description: "Denim. Made in Japan")
 let product2 = Product(name: "Pants", size: 3, color: "Black", description: "100% Cotton")
 let product3 = Product(name: "T-Shirt", size: 4, color: "White", description: "100% Jersey Cotton")
-let product4 = Product(name: "Tank Top", size: 4, color: "Walnut", description: nil)
+let product4 = Product(name: "Mules", size: 38, color: "Walnut", description: nil)
 let product5 = Product(name: "Hat", size: 2, color: "Forest Green", description: nil)
 let product6 = Product(name: "Hoodie", size: 4, color: "Black", description: "100% French Terry Cotton")
 
@@ -40,19 +53,3 @@ func printElements(products: [Product]) {
 
 printElements(products: myProductList)
 
-// Above & Beyond
-var myShoeList = [Product]()
-
-let shoe1 = Product(name: "Sneaker", size: 40, color: "Indigo", description: nil)
-let shoe2 = Product(name: "Loafers", size: 38, color: "Black", description: nil)
-let shoe3 = Product(name: "Mules", size: 42, color: "White", description: "100% Leather")
-
-func addShoe(product: Product) {
-  myShoeList.append(product)
-}
-
-addShoe(product: shoe1)
-addShoe(product: shoe2)
-addShoe(product: shoe3)
-
-printElements(products: myShoeList)
