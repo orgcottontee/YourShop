6e2ec793fa07242d77728acd1023223a79688fa2
catherine <h1codingqueenh1@gmail.com>
2022-09-21 11:18:36 -0700

    Adjusted Category and SubCategory data

--- yu-shop-catherine/yu-shop-catherine/Models/Category.swift
+++ yu-shop-catherine/yu-shop-catherine/Models/Category.swift
@@ -9,41 +9,38 @@ import Foundation
 
 struct Category: Identifiable {
   let id = UUID()
+  let name: String
+  
   struct SubCategory {
     let categoryName: String
     let name: String
   }
   
-  let name: String
   let subCategories: [SubCategory]
 }
 
-extension Category {
-  static let categoryList = [
-    (name: "Bottoms", SubCategory: [
-      "Pants",
-      "Trousers",
-      "Skirt",
-      "Shorts",
-    ]),
-    (name: "Tops", SubCategory: [
-      "Tshirt",
-      "Puffer",
-      "Hoodie",
-      "Jacket"
-    ]),
-    (name: "Accessories", SubCategory: [
-      "Necklace",
-      "Earrings",
-      "Rings",
-      "Socks"
-    ]),
-    (name: "Footwear", SubCategory: [
-      "Sneakers",
-      "Mules",
-      "Slides",
-      "Boots"
-    ]),
-  ]
-}
+let bottomsCategory = Category(name: "Bottoms", subCategories: [
+  Category.SubCategory(categoryName: "Bottoms", name: "Pants"),
+  Category.SubCategory(categoryName: "Bottoms", name: "Jeans"),
+  Category.SubCategory(categoryName: "Bottoms", name: "Shorts"),
+])
 
+let topsCategory = Category(name: "Tops", subCategories: [
+  Category.SubCategory(categoryName: "Tops", name: "T-shirt"),
+  Category.SubCategory(categoryName: "Tops", name: "Sweater"),
+  Category.SubCategory(categoryName: "Tops", name: "Outerwear"),
+])
+
+let accessoriesCategory = Category(name: "Accessories", subCategories: [
+  Category.SubCategory(categoryName: "Accessories", name: "Socks"),
+  Category.SubCategory(categoryName: "Accessories", name: "Jewelry"),
+  Category.SubCategory(categoryName: "Accessories", name: "Hats"),
+])
+
+let footwearCategory = Category(name: "Footwear", subCategories: [
+  Category.SubCategory(categoryName: "Footwear", name: "Mules"),
+  Category.SubCategory(categoryName: "Footwear", name: "Boots"),
+  Category.SubCategory(categoryName: "Footwear", name: "Sneakers"),
+])
+
+var categoryList = [bottomsCategory, topsCategory, accessoriesCategory, footwearCategory]
