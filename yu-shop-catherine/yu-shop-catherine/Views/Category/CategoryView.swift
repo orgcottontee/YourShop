c328545b4587d16c5580d0272776211f96429e96
catherine <h1codingqueenh1@gmail.com>
2022-10-10 13:08:07 +0800

    Added cookies assignment into CategoryView

--- yu-shop-catherine/yu-shop-catherine/Views/Category/CategoryView.swift
+++ yu-shop-catherine/yu-shop-catherine/Views/Category/CategoryView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct CategoryView: View {
+  enum APIError: Error {
+    case requestFailed
+    case responseDecodingFailed
+    case urlCreationFailed
+  }
   
   var columns = [GridItem(.flexible())]
   var categories: [Category]
@@ -19,12 +24,26 @@ struct CategoryView: View {
       VStack(alignment: .leading) {
         Text("Yu Shop")
           .kerning(4)
-          .rotationEffect(Angle(degrees: -90))
+          .padding()
           .font(.largeTitle.bold())
         ScrollView(.horizontal) {
           LazyHGrid(rows: columns, spacing: 20) {
             ForEach(categories, id: \.id) { category in
               CategoryCard(category: category)
+            }
+            .task {
+              do {
+                let cookies = try await fetchCookies()
+                print(cookies)
+              } catch APIError.requestFailed {
+                print("Your request failed")
+              } catch APIError.responseDecodingFailed {
+                print("Failed response")
+              } catch APIError.urlCreationFailed {
+                print("Invalid URL")
+              } catch {
+                print(error.localizedDescription)
+              }
             }
             // NavigationLink(destination: ProductDetailView(product: product)) {
             // Forward to CategoryDetailView(not yet created) when users click
@@ -33,6 +52,31 @@ struct CategoryView: View {
         .padding()
       }
     }
+  }
+  private func fetchCookies() async throws -> String {
+    
+    guard let url = URL(string: "https://www.raywenderlich.com") else {
+      throw APIError.urlCreationFailed
+    }
+    let (_, response) = try await URLSession.shared.data(from: url)
+    
+    guard let httpResponse = response as? HTTPURLResponse,
+          let fields = httpResponse.allHeaderFields as? [String: String]
+    else {
+      throw APIError.requestFailed
+    }
+    
+    let cookies = HTTPCookie.cookies(withResponseHeaderFields: fields, for: url)
+    
+    guard !cookies.isEmpty else {
+      return "No cookies"
+    }
+    var descriptionString = ""
+    
+    for cookie in cookies {
+      descriptionString += "\(cookie.name): \(cookie.value)\n"
+    }
+    return descriptionString
   }
 }
 
